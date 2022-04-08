89c543120b3f1f85b6e33b5042c8eae3e8a0f274
aoviedogh <aoviedo2701@gmail.com>
2022-04-07 21:36:48 -0500

    cuarto commit clase 30/2

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/repository/UserRepository.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/repository/UserRepository.java
@@ -5,6 +5,7 @@ import com.fundamentosplatzi.springboot.fundamentos.entity.User;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +14,8 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+//public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.email=?1")
     Optional<User> findByUserEmail(String email);
@@ -32,4 +34,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<UserDto> getAllByCumpleanosAndEmail(@Param("parametroFecha") LocalDate date,
                                                  @Param("parametroEmail") String email);
     */
+
+    List<User> findAll();
+
 }
